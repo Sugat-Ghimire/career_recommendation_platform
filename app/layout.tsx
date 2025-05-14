3443d029cc50789687565a85740e7a9ab93ee9d3
Sugat Ghimire <sugat1237@gmail.com>
2025-05-14 22:40:30 +0545

    added quicksand font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const quicksand = Quicksand({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // Choose weights you need
+  variable: "--font-quicksand", // Optional: for Tailwind integration
+  display: "swap", // Optional: improves performance
 });
 
 export const metadata: Metadata = {
@@ -25,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
-      >
-        {children}
-      </body>
+      <body className={`${quicksand.className}`}>{children}</body>
     </html>
   );
 }
